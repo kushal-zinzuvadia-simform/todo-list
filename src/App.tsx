a25bdbac7c4c8c89eb44fcd799e5bee6ae97508f
Kushal Zinzuvadia <kushal.zinzuvadia@simformsolutions.com>
2026-06-01 19:08:44 +0530

    feat: Show added todos in a table format

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'react-hot-toast';
 import InputForm from './components/InputForm/InputForm';
 import { useTodoData } from './hooks/useTodoData';
 import { useEffect } from 'react';
+import { TodoList } from './components/TodoList/TodoList';
 
 function App() {
   const { todoData, addItem } = useTodoData();
@@ -13,6 +14,7 @@ function App() {
       <Toaster />
       <h2 className="text-2xl font-semibold">My To-Do List</h2>
       <InputForm onAdd={addItem} />
+      <TodoList todos={todoData} />
     </div>
   );
 }
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,54 @@
+import type { Todo } from '../../types/TodoItem';
+
+type TodoListProps = {
+  todos: Todo[];
+};
+
+export const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <div className="mx-auto w-full max-w-175 overflow-hidden border rounded-lg">
+      <table className="w-full table-fixed">
+        <thead>
+          <tr className="border-b bg-gray-50">
+            <th className="w-[10%] px-4 py-2 text-left"></th>
+            <th className="w-[50%] px-4 py-2 text-left">Task</th>
+            <th className="w-[30%] px-4 py-2 text-left">Date Added</th>
+            <th className="w-[10%] px-4 py-2 text-left"></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {todos.length === 0 ? (
+            <tr>
+              <td colSpan={4} className="px-4 py-8 text-center text-gray-500">
+                No Tasks added yet.
+              </td>
+            </tr>
+          ) : (
+            todos.map((todo, index) => (
+              <tr
+                key={`${todo.text}-${index}`}
+                className="border-b last:border-b-0 even:bg-gray-100"
+              >
+                <td className="px-4 py-2">
+                  <input
+                    type="checkbox"
+                    name="CheckTodo"
+                    className="h-4 w-4 cursor-pointer rounded border-gray-300 accent-blue-600"
+                  />
+                </td>
+                <td className="px-4 py-2">{todo.text}</td>
+                <td className="px-4 py-2">{todo.createdAt}</td>
+                <td className="px-4 py-2">
+                  <button className="cursor-pointer rounded p-1 hover:bg-red-100">
+                    <img src="/delete.svg" alt="Delete" className="h-5 w-5" />
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
